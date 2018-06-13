cdc78d9753809917d01bf783d6ddd72b1fbc51a5
huangzhijun <huangzhijun@oaserver.dw.gdbbk.com>
2018-06-13 15:31:22 +0800

    添加屏幕分辨率兼容适配

--- app/src/main/java/com/hzj/mytest/screensize/TestScreenAdapterActivity.java
+++ app/src/main/java/com/hzj/mytest/screensize/TestScreenAdapterActivity.java
@@ -3,6 +3,8 @@ package com.hzj.mytest.screensize;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
+import android.widget.TextView;
 
 import com.hzj.mytest.R;
 import com.hzj.mytest.util.ScreenUtil;
@@ -11,19 +13,27 @@ public class TestScreenAdapterActivity extends Activity {
 
     private static final String TAG = "TestScreenAdapter：";
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_adapter);
 
-        Log.i(TAG, "getScreenHeight: " + ScreenUtil.getScreenHeight(this));
+        textView = (TextView) findViewById(R.id.screen_data);
+
         Log.i(TAG, "getPxHeight: " + ScreenUtil.getPxHeight(this));
         Log.i(TAG, "getPxWidth: " + ScreenUtil.getPxWidth(this));
         Log.i(TAG, "getScreenDensity: " + ScreenUtil.getScreenDensity(this));
 
+        Display mDisplay = getWindowManager().getDefaultDisplay();
+
+        textView.setText("Screen Ratio: ["
+                + ScreenUtil.getPxWidth(this) + "x" + ScreenUtil.getPxHeight(this) + "]"
+                + "\n\n" + "Screen Display: " + mDisplay.toString());
+
         ScreenUtil.getScreenDensity_ByWindowManager(this);
         ScreenUtil.getScreenDensity_ByResources(this);
-        ScreenUtil.getDefaultScreenDensity(this);
     }
 
 }
